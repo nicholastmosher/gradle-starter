8e6587248803727ba9c805b86c501ca46647bca2
Nick Mosher <nicholastmosher@gmail.com>
2019-07-26 21:31:11 -0400

    Initial commit

---
+++ src/test/com/example/MainTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testTrue() {
+        Assert.assertTrue(true);
+    }
+}
