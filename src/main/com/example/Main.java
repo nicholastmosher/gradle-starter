952836dd78cf994960a0f154f40915c1255afe26
Nick Mosher <nicholastmosher@gmail.com>
2019-07-26 22:28:31 -0400

    Add picocli command-line parser

--- src/main/com/example/Main.java
+++ src/main/com/example/Main.java
@@ -1,7 +1,24 @@
 package com.example;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "greeter", description = "Say hello in many different ways!")
 public class Main {
+
+    @CommandLine.Option(names = {"--name"}, description = "The name of whomst to greet!")
+    private String name = "World";
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Main application = new Main();
+        CommandLine cmd = new CommandLine(application);
+
+        try {
+            cmd.parseArgs(args);
+        } catch (Exception e) {
+            cmd.usage(System.out);
+            return;
+        }
+
+        System.out.printf("Hello, %s!\n", application.name);
     }
 }
